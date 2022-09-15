0c35becbe41e43beb4b319486a089a129b8657fa
N4choCM <nachocamposmarti@gmail.com>
2022-09-15 03:01:50 +0200

    App documentation ready!

--- src/EmailGenerator.java
+++ src/EmailGenerator.java
@@ -12,18 +12,14 @@ public class EmailGenerator {
     private String sAlternateEmail;
     private String sCompanySuffix = "codingschool.com";
 
-    // Constructor that receives the first and last names.
     public EmailGenerator(){
 
         this.sFullName = setFullName();
 
-        // Call a method asking for the department - return the department.
         this.sDepartment = setDepartment();
 
-        // Call a method returning a random password.
         this.sPassword = generateRandomPassword(iDefaultPasswordLength);
 
-        // Combine elements to generate Email address.
         sEmail = sFullName.toLowerCase() + "@" + sDepartment + "." + sCompanySuffix;
 
         setAlternateEmail();
@@ -34,7 +30,12 @@ public class EmailGenerator {
 
     }
 
-    //Ask for the name
+    /**
+     * Method that sets the First Name, the Last Name and the Full Name of the new worker.
+      * @return A String concatenation with the First Name and the Last Name.
+     * @author Nacho Campos Martí
+     * @since 15/09/2022
+     */
     private String setFullName(){
         System.out.println("Please, enter your first name: ");
         Scanner firstName = new Scanner(System.in);
@@ -49,7 +50,12 @@ public class EmailGenerator {
         return sFirstName + sLastName;
     }
 
-    // Ask for the department
+    /**
+     * Method that establishes the department of the worker. This will be used when creating the new email address.
+      * @return A String with the reference of the department.
+     * @author Nacho Campos Martí
+     * @since 15/09/2022
+     */
     private String setDepartment(){
         System.out.println("\nWelcome to Coding School! You will find below our Department Codes." +
                 "\n----------------------------------------------------------------------------" +
@@ -67,6 +73,14 @@ public class EmailGenerator {
         return iDepartmentCode == 1 ? "back" : iDepartmentCode == 2 ? "front" : iDepartmentCode == 3 ? "fs" : "general";
     }
 
+    /**
+     * Method that generates a random password for the worker's account.
+     * @param iLength Directly connected to the param iDefaultPasswordLength, which has been set by default to 30.
+     *                It will be the number of characters that the random password will have.
+     * @return A String with the random password.
+     * @author Nacho Campos Martí
+     * @since 15/09/2022
+     */
     private String generateRandomPassword(int iLength){
         String sPasswordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-/!@#~$%";
         char[] cPassword = new char[iLength];
@@ -78,10 +92,22 @@ public class EmailGenerator {
         return new String(cPassword);
     }
 
+    /**
+     * Method that sets the Mailbox Capacity. It is not used since it has been set 1 GB by default, but the method
+     * exists in case it would be necessary to change this capacity in the future.
+     * @param iMailBoxCapacity Directly connected to this.iMailBoxCapacity, which has been set by default to 1 GB.
+     * @author Nacho Campos Martí
+     * @since 15/09/2022
+     */
     public void setMailBoxCapacity(int iMailBoxCapacity){
         this.iMailBoxCapacity = iMailBoxCapacity;
     }
 
+    /**
+     * Method that asks the user to set an alternative email address.
+     * @author Nacho Campos Martí
+     * @since 15/09/2022
+     */
     public void setAlternateEmail() {
         System.out.println("\nPlease, enter an alternative email address: ");
         Scanner alternateEmail = new Scanner(System.in);
@@ -90,12 +116,31 @@ public class EmailGenerator {
         this.sAlternateEmail = sAlternateEmail;
     }
 
+    /**
+     * Method that converts the random generated password into a new one introduced by the user.
+     * @author Nacho Campos Martí
+     * @since 15/09/2022
+     */
     public void changePassword(){
         System.out.println("\nPlease, you must change your password immediately for security purposes: ");
         Scanner password = new Scanner(System.in);
         String sPassword = password.next().replace(" ", "");
         this.sPassword = sPassword;
         System.out.println("\nYour new password is: " + sPassword);
+    }
+
+    /**
+     * Method that shows to the user his/her personal information.
+     * @return String with his/her personal information.
+     * @author Nacho Campos Martí
+     * @since 15/09/2022
+     */
+    public String showInfo(){
+        return "\nFull name: " + sFirstName + " " + sLastName +
+                "\nCompany email: " + sEmail +
+                "\nAlternate email: " + sAlternateEmail +
+                "\nMailbox capacity: " + iMailBoxCapacity + " " + "GB" +
+                "\nYour automatically generated password is: " + sPassword;
     }
 
     public String getPassword() {
@@ -108,13 +153,5 @@ public class EmailGenerator {
 
     public String getAlternateEmail() {
         return sAlternateEmail;
-    }
-
-    public String showInfo(){
-        return "\nFull name: " + sFirstName + " " + sLastName +
-                "\nCompany email: " + sEmail +
-                "\nAlternate email: " + sAlternateEmail +
-                "\nMailbox capacity: " + iMailBoxCapacity + " " + "GB" +
-                "\nYour automatically generated password is: " + sPassword;
     }
 }
